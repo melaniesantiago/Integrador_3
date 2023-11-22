c20198c1c010abc967059fa41a92657bbe773024
melanie santiago resendiz <230110616@itsoeh.edu.mx>
2023-11-22 14:39:02 -0600

    se agrego mas codigo a mi ejercicio

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class ejercicio2 {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ingrese los coeficientes de la ecuación cuadrática:");
+    System.out.print("Coeficiente a: ");
+    double a = scanner.nextDouble();
+    System.out.println("Coeficiente b: ");
+    double b = scanner.nextDouble();
+    System.out.print("Coeficiente c: ");
+    double c = scanner.nextDouble();
 
 
 
